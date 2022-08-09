52516ef24faedc875fc4df955e7f14f6f29b33b3
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-08-08 21:50:05 -0300

    Auth, register, and styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { createServer } = require('http')
 const path = require('path')
+const cookieParser = require('cookie-parser')
 const realtimeServer = require('./realtimeServer')
 
 const app = express()
@@ -9,6 +10,7 @@ const httpServer = createServer(app)
 // Settings
 app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'views'))
+app.use(cookieParser())
 
 // Routes
 app.use(require('./routes'))
